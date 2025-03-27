27260353283d77d0bb5138d922b86751338e3b86
justajoo0374 <justajoo0374@gmail.com>
2025-03-27 12:17:07 +0530

    Update main.tf

--- modules/compute-instances/main.tf
+++ modules/compute-instances/main.tf
@@ -11,7 +11,7 @@ resource "google_compute_instance" "default" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = var.image
       labels = {
         my_label = "value"
       }
