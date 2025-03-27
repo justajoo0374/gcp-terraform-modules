e919baab6a3887d56a72e34293ce67e04b71a3bc
justajoo0374 <justajoo0374@gmail.com>
2025-03-27 08:45:34 +0530

    Create main.tf

---
+++ modules/compute-instances/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.27.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+}
+
+resource "google_compute_instance" "default" {
+  name         = "my-instance"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+  network_interface {
+    network = "default"
+}
+}
