f750867996b23dcaf9451895da2223240ab7aadc
justajoo0374 <justajoo0374@gmail.com>
2025-03-27 10:08:14 +0530

    Update main.tf

--- modules/compute-instances/main.tf
+++ modules/compute-instances/main.tf
@@ -4,7 +4,7 @@ provider "google" {
 }
 
 resource "google_compute_instance" "default" {
-  name         = "my-instance"
+  name         = "my-instance1"
   machine_type = "n2-standard-2"
   zone         = "us-central1-a"
   project = "magnetic-flare-454316-h0"
